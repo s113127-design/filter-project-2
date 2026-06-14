8dd4ac461b75b949ecd713107d624927a33111dc
113127 <s113127@ms2.kghs.kh.edu.tw>
2026-06-14 15:29:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ class VideoProcessor:
                     cap_w = int(face_width * 2.2)
                     cap_scale = kongzi_cap.shape[0] / kongzi_cap.shape[1]
                     cap_h = int(cap_w * cap_scale)
-                    cap_x = int(forehead.x * w - cap_w / 2)
+                    cap_x = int(forehead.x * w - cap_w / 1.9)
                     cap_y = int(forehead.y * h - cap_h * 0.45)
                     img = overlay_image(img, kongzi_cap, cap_x, cap_y, size=(cap_w, cap_h))
                     
